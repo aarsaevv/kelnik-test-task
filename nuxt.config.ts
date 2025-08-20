f96d3f4c6201e75c5e5be3da746d8c1fe84c930f
Evgeny Arsaev <vltby000@gmail.com>
2025-08-21 00:17:09 +0700

    init(app): add essential packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   routeRules: {
     // prerender index route by default
-    '/': { prerender: true },
+    "/": { prerender: true },
   },
+  compatibilityDate: "latest",
 });
